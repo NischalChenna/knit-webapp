71ef062ded14582b506008f7c27d60188b28bfd9
piyush-sb <piyush@superbeings.ai>
2023-02-13 22:24:44 +0530

    feat : Update table skeleton

--- src/components/TableSkeleton/index.tsx
+++ src/components/TableSkeleton/index.tsx
@@ -1,11 +1,38 @@
-import { Skeleton } from "antd";
-
+import { Col, Row, Skeleton } from "antd";
+import React from "react";
+import "./TableSkeleton.scss";
 interface Skeleton {
   rows: number;
 }
 
-const TableSkeleton = (props: Skeleton): JSX.Element => {
-  return <Skeleton active paragraph={{ rows: props.rows }} />;
+const TableSkeleton = ({ rows = 5 }: Skeleton): JSX.Element => {
+  return (
+    <React.Fragment>
+      {" "}
+      {[...Array(rows).keys()].map((r, i) => (
+        <Row key={i} className="table-skeleton">
+          <Col sm={6} md={6} className="px-1">
+            <Skeleton.Button active={i % 2 ==0} size="large" />
+          </Col>{" "}
+          <Col sm={6} md={6} className="px-1">
+            <Skeleton.Button active={i % 2 ==0} size="large" />
+          </Col>{" "}
+          <Col sm={6} md={6} className="px-1">
+            <Skeleton.Button active={i % 2 ==0} size="large" />
+          </Col>{" "}
+          <Col sm={6} md={6} className="px-1">
+            <Skeleton.Button active={i % 2 ==0} size="large" />
+          </Col>{" "}
+          {/* <Col sm={4} md={4} className="px-1">
+            <Skeleton.Button active size="large" />
+          </Col>{" "}
+          <Col sm={4} md={4} className="px-1">
+            <Skeleton.Button active size="large" />
+          </Col>{" "} */}
+        </Row>
+      ))}
+    </React.Fragment>
+  );
 };
 
 export default TableSkeleton;
