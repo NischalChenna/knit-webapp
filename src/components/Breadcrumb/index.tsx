5ce871d72199f6fa134598710a89618624169c28
NischalChenna <chennanischal5000@gmail.com>
2023-02-16 00:45:41 +0530

    fix 5

--- src/components/Breadcrumb/index.tsx
+++ src/components/Breadcrumb/index.tsx
@@ -11,11 +11,11 @@ const DashBreadCrumb = () => {
   const pathSnippets = location.pathname.split("/").filter((i) => i);
   return (
     <Breadcrumb style={{ fontWeight: "bold" }} separator={`>`}>
-      <Breadcrumb.Item key="home" className="py-2">
-        <Link className="text-decoration-none" to="/dashboard/home">
-          <Icon path={mdiHomeOutline} size={1} />
-        </Link>
-      </Breadcrumb.Item>
+      {/* <Breadcrumb.Item key="home" className="py-2"> */}
+      <Link className="text-decoration-none" to="/dashboard/home" key={"home"}>
+        <Icon path={mdiHomeOutline} size={1} />
+      </Link>
+      {/* </Breadcrumb.Item> */}
       {pathSnippets.map((_, index) => {
         if (index > 0) {
           const url = `/${pathSnippets.slice(0, index + 1).join("/")}`;
@@ -29,17 +29,17 @@ const DashBreadCrumb = () => {
             );
             if (_ != "home")
               return (
-                <Breadcrumb.Item key={url} className="py-2">
-                  <Link className="text-decoration-none" to={url}>
-                    {crumbRouteObj?.label}
-                    {crumbRouteObj?.labelParams
-                      ? " : " +
-                        matchedRoute[0]["params"][
-                          crumbRouteObj?.labelParams as string
-                        ]
-                      : null}
-                  </Link>
-                </Breadcrumb.Item>
+                // <Breadcrumb.Item key={url} className="py-2">
+                <Link className="text-decoration-none" to={url} key={url}>
+                  {crumbRouteObj?.label}
+                  {crumbRouteObj?.labelParams
+                    ? " : " +
+                      matchedRoute[0]["params"][
+                        crumbRouteObj?.labelParams as string
+                      ]
+                    : null}
+                </Link>
+                // </Breadcrumb.Item>
               );
           }
         }
