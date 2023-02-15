7c85c2f1672259163a1916a24ab84672dd79d63d
NischalChenna <chennanischal5000@gmail.com>
2023-02-15 23:56:27 +0530

    fix spacing

--- src/components/Breadcrumb/index.tsx
+++ src/components/Breadcrumb/index.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Breadcrumb } from "antd";
 import { useLocation, Link, matchRoutes } from "react-router-dom";
 import { mdiHomeOutline } from "@mdi/js";
@@ -10,52 +9,45 @@ const DashBreadCrumb = () => {
   const location = useLocation();
   console.log("locatuionObj", location);
   const pathSnippets = location.pathname.split("/").filter((i) => i);
-  const extraBreadcrumbItems = pathSnippets.map((_, index) => {
-    if (index > 0) {
-      const url = `/${pathSnippets.slice(0, index + 1).join("/")}`;
-      const pathObjArray = getDashboardRoutes(isFirstLogin).map((path) => {
-        return { path: path.path };
-      });
-      const matchedRoute = matchRoutes(pathObjArray, url);
-
-      if (matchedRoute && matchedRoute.length) {
-        const crumbRouteObj = getDashboardRoutes(isFirstLogin).find(
-          (routeObj) => routeObj.path == matchedRoute[0].route.path
-        );
-
-        if (_ != "home")
-          return (
-            <Breadcrumb.Item key={url} className="py-2">
-              <Link className="text-decoration-none" to={url}>
-                {crumbRouteObj?.label}
-                {crumbRouteObj?.labelParams
-                  ? " : " +
-                    matchedRoute[0]["params"][
-                      crumbRouteObj?.labelParams as string
-                    ]
-                  : null}
-              </Link>
-            </Breadcrumb.Item>
-          );
-      }
-    }
-  });
-  const breadcrumbItems = [
-    <Breadcrumb.Item key="home" className="py-2">
-      <Link className="text-decoration-none" to="/dashboard/home">
-        <Icon path={mdiHomeOutline} size={1} />
-      </Link>
-    </Breadcrumb.Item>,
-  ].concat(extraBreadcrumbItems as JSX.Element[]);
-
   return (
     <Breadcrumb
       style={{ fontWeight: "bold" }}
       separator={<span className="fw-bolder">{`>`}</span>}
     >
-      {breadcrumbItems}
+      <Breadcrumb.Item key="home" className="py-2">
+        <Link className="text-decoration-none" to="/dashboard/home">
+          <Icon path={mdiHomeOutline} size={1} />
+        </Link>
+      </Breadcrumb.Item>
+      {pathSnippets.map((_, index) => {
+        if (index > 0) {
+          const url = `/${pathSnippets.slice(0, index + 1).join("/")}`;
+          const pathObjArray = getDashboardRoutes(isFirstLogin).map((path) => {
+            return { path: path.path };
+          });
+          const matchedRoute = matchRoutes(pathObjArray, url);
+          if (matchedRoute && matchedRoute.length) {
+            const crumbRouteObj = getDashboardRoutes(isFirstLogin).find(
+              (routeObj) => routeObj.path == matchedRoute[0].route.path
+            );
+            if (_ != "home")
+              return (
+                <Breadcrumb.Item key={url} className="py-2">
+                  <Link className="text-decoration-none" to={url}>
+                    {crumbRouteObj?.label}
+                    {crumbRouteObj?.labelParams
+                      ? " : " +
+                        matchedRoute[0]["params"][
+                          crumbRouteObj?.labelParams as string
+                        ]
+                      : null}
+                  </Link>
+                </Breadcrumb.Item>
+              );
+          }
+        }
+      })}
     </Breadcrumb>
   );
 };
-
 export default DashBreadCrumb;
